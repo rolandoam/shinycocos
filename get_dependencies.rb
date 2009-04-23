563189807b47bb0c9ae97145859a616b3a06b717
Rolando Abarca <rolando@gamesforfood.cl>
2009-04-23 11:07:43 -0400

    updateing get_dependencies script

--- get_dependencies.rb
+++ get_dependencies.rb
@@ -15,12 +15,22 @@ end
 
 # get ruby 1.9
 do_shell "downloading ruby 1.9..." do
+  ruby_tar = RUBY_1_9_1.split('/').last
+  rm ruby_tar if File.exists?(ruby_tar)
   system("curl -sLO '#{RUBY_1_9_1}' > /dev/null")
 end
 
 # checking out cocos-2d
 do_shell "checking out cocos-2d..." do
-  system("svn co '#{COCOS_2D_IPHONE}' cocos2d-iphone > /dev/null")
+  if File.exists?("cocos2d-iphone")
+    # ok, the directory exists, check if it's a subversion checkout, if so, update
+    # else... keep it that way
+    if File.exists?("cocos2d-iphone/.svn")
+      system("cd cocos2d-iphone; svn up > /dev/null")
+    end
+  else
+    system("svn co '#{COCOS_2D_IPHONE}' cocos2d-iphone > /dev/null")
+  end
 end
 
 # configure ruby 1.9
@@ -35,5 +45,5 @@ do_shell "configuring ruby 1.9..." do
 end
 
 do_shell "making ruby 1.9 (we need some auto-generated files)..." do
-  #system("cd ruby; make > /dev/null")
+  system("cd ruby; make > /dev/null")
 end
