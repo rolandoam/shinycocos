29724811e5c79f6a0442dd22429a0c35ba7704a7
Rolando Abarca <rolando@gamesforfood.cl>
2009-04-23 11:08:41 -0400

    added common_sc_make_rect

--- Integration/SC_common.h
+++ Integration/SC_common.h
@@ -26,3 +26,16 @@ VALUE common_init(VALUE klass, cocos_holder *ptr, BOOL release_on_free);
 VALUE common_rb_ns_log(int argc, VALUE *argv, VALUE module);
 VALUE common_rb_set_acceleration_delegate(VALUE module, VALUE obj);
 void common_method_swap(Class cls, SEL orig, SEL repl, const char *signature);
+
+static inline CGRect common_sc_make_rect(VALUE rb_rect) {
+	Check_Type(rb_rect, T_ARRAY);
+	if (FIX2INT(rb_funcall(rb_rect, rb_intern("length"), 0)) < 4) {
+		rb_raise(rb_eArgError, "rect must be of at least 4 elements");
+	}
+	return CGRectMake(
+		NUM2DBL(rb_ary_entry(rb_rect, 0)),
+		NUM2DBL(rb_ary_entry(rb_rect, 1)),
+		NUM2DBL(rb_ary_entry(rb_rect, 2)),
+		NUM2DBL(rb_ary_entry(rb_rect, 3))
+	);
+}
