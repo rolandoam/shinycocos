fa486809e5d1d787b24126aa36722af8adb46ece
Rolando Abarca <rolando@gamesforfood.cl>
2009-08-05 13:17:48 -0400

    adding functionality to TMXLayer

--- Integration/SC_TMXTiledMap.m
+++ Integration/SC_TMXTiledMap.m
@@ -20,6 +20,7 @@
 #import "SC_common.h"
 #import "SC_CocosNode.h"
 #import "SC_AtlasSpriteManager.h"
+#import "SC_AtlasSprite.h"
 #import "SC_TMXTiledMap.h"
 
 VALUE rb_cTMXTiledMap;
@@ -66,6 +67,122 @@ void init_rb_cTMXTiledMap() {
 	rb_define_method(rb_cTMXTiledMap, "[]", rb_cTMXTiledMap_get_layer, 1);
 }
 
+
+/*
+ * call-seq:
+ *   layer.name   #=> string
+ *
+ * Returns the layer's name, as defined in Tiled
+ */
+VALUE rb_cTMXLayer_name(VALUE obj) {
+	return rb_str_new2([CC_TMXLAYER(obj).layerName cStringUsingEncoding:NSUTF8StringEncoding]);
+}
+
+
+/*
+ * call-seq:
+ *   layer.add_tile(gid, coord)   #=> AtlasSprite
+ *
+ * Adds a new tile for the given gid on the specified position (as a 2d array).
+ * Returns a new AtlasSprite.
+ */
+VALUE rb_cTMXLayer_add_tile(VALUE obj, VALUE gid, VALUE coord) {
+	Check_Type(gid, T_FIXNUM);
+	Check_Type(coord, T_ARRAY);
+	
+	CGPoint pos = CGPointMake(FIX2INT(RARRAY_PTR(coord)[0]), FIX2INT(RARRAY_PTR(coord)[1]));
+	AtlasSprite *sprite = [CC_TMXLAYER(obj) addTileForGID:FIX2INT(gid) at:pos];
+	if (!sprite.userData) {
+		VALUE ret = sc_init(rb_cAtlasSprite, nil, sprite, 0, 0, YES);
+		sprite.userData = (void *)ret;
+	}
+	return (VALUE)sprite.userData;
+}
+
+
+/*
+ * call-seq:
+ *   layer.tile(coord)   #=> AtlasSprite
+ *
+ * Returns the tile (as an AtlasSprite) for the given coordinate (as a 2d array)
+ */
+VALUE rb_cTMXLayer_tile(VALUE obj, VALUE coord) {
+	Check_Type(coord, T_ARRAY);
+	CGPoint pos = CGPointMake(FIX2INT(RARRAY_PTR(coord)[0]), FIX2INT(RARRAY_PTR(coord)[1]));
+	AtlasSprite *sprite = [CC_TMXLAYER(obj) tileAt:pos];
+	if (!sprite.userData) {
+		VALUE ret = sc_init(rb_cAtlasSprite, nil, sprite, 0, 0, YES);
+		sprite.userData = (void *)ret;
+	}
+	return (VALUE)sprite.userData;
+}
+
+
+/*
+ * call-seq:
+ *   layer.tile_gid(coord)   #=> int
+ *
+ * Same as layer.tile, but returns the gid of the tile instead of an AtlasSprite
+ */
+VALUE rb_cTMXLayer_tile_gid(VALUE obj, VALUE coord) {
+	Check_Type(coord, T_ARRAY);
+	CGPoint pos = CGPointMake(FIX2INT(RARRAY_PTR(coord)[0]), FIX2INT(RARRAY_PTR(coord)[1]));
+	unsigned int gid = [CC_TMXLAYER(obj) tileGIDAt:pos];
+	return INT2FIX(gid);
+}
+
+
+/*
+ * call-seq:
+ *   layer.set_tile_gid(gid, coord)   #=> gid
+ *
+ * Sets the given tile (based on the gid) for the specified position (as a 2d array)
+ */
+VALUE rb_cTMXLayer_set_tile_gid(VALUE obj, VALUE gid, VALUE coord) {
+	Check_Type(coord, T_ARRAY);
+	CGPoint pos = CGPointMake(FIX2INT(RARRAY_PTR(coord)[0]), FIX2INT(RARRAY_PTR(coord)[1]));
+	[CC_TMXLAYER(obj) setTileGID:FIX2INT(gid) at:pos];
+	return gid;
+}
+
+
+/*
+ * call-seq:
+ *   layer.remove_tile(coord)   #=> nil
+ *
+ * Removes the tile at the specified coordinate (as a 2d array)
+ */
+VALUE rb_cTMXLayer_remove_tile(VALUE obj, VALUE coord) {
+	Check_Type(coord, T_ARRAY);
+	CGPoint pos = CGPointMake(FIX2INT(RARRAY_PTR(coord)[0]), FIX2INT(RARRAY_PTR(coord)[1]));
+	[CC_TMXLAYER(obj) removeTileAt:pos];
+	return Qnil;
+}
+
+
+/*
+ * call-seq:
+ *   layer.px_position(coord)   #=> [x,y]
+ *
+ * Returns the position in pixels of a given tile coordinate
+ */
+VALUE rb_cTMXLayer_px_position(VALUE obj, VALUE coord) {
+	Check_Type(coord, T_ARRAY);
+	CGPoint pos = CGPointMake(FIX2INT(RARRAY_PTR(coord)[0]), FIX2INT(RARRAY_PTR(coord)[1]));
+	CGPoint pxpos = [CC_TMXLAYER(obj) positionAt:pos];
+	return rb_ary_new3(2, rb_float_new(pxpos.x), rb_float_new(pxpos.y));
+}
+
+
 void init_rb_cTMXLayer() {
 	rb_cTMXLayer = rb_define_class_under(rb_mCocos2D, "TMXLayer", rb_cAtlasSpriteManager);
+	// properties
+	rb_define_method(rb_cTMXLayer, "name", rb_cTMXLayer_name, 0);
+	// methods
+	rb_define_method(rb_cTMXLayer, "add_tile", rb_cTMXLayer_add_tile, 2);
+	rb_define_method(rb_cTMXLayer, "tile", rb_cTMXLayer_tile, 1);
+	rb_define_method(rb_cTMXLayer, "tile_gid", rb_cTMXLayer_tile_gid, 1);
+	rb_define_method(rb_cTMXLayer, "set_tile_gid", rb_cTMXLayer_set_tile_gid, 2);
+	rb_define_method(rb_cTMXLayer, "remove_tile", rb_cTMXLayer_remove_tile, 1);
+	rb_define_method(rb_cTMXLayer, "px_position", rb_cTMXLayer_px_position, 1);
 }
